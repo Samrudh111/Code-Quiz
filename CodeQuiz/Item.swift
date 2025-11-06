ebc8dc9c53c9874905a35838f66d8ff4e9500e08
Sam <samrudhsurendra555@gmail.com>
2025-11-06 17:20:56 -0600

    Initial Commit

---
+++ CodeQuiz/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CodeQuiz
+//
+//  Created by Samrudh S on 11/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
